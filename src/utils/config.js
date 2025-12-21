a842b29debeb2cc52303705bb5b1ad7b89f6aaa9
USHAKIRAN BUDDALA <buddalaushakiran7@gmail.com>
2025-12-21 14:11:12 +0530

    Fix backend URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "https://attendence-backend-mc9j.onrender.com";
 export default API_BASE;
